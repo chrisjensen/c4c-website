6ef3859b5a5c69ef11afaa903c29fbec16c1408d
Chris Jensen <chris@goodforus.info>
2015-12-09 15:17:49 +1100

    Test google spreadsheet referencing.

--- app/theme/climateforchangetrial-565d2772ebad6419ed000001/actions_controllers.js
+++ app/theme/climateforchangetrial-565d2772ebad6419ed000001/actions_controllers.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('c4cWebsite.actions', ['ngRoute'])
+(function() {
+
+angular.module('c4cWebsite.actions', ['ngRoute', 'times.tabletop'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
@@ -13,14 +15,29 @@ angular.module('c4cWebsite.actions', ['ngRoute'])
   });
 }])
 
-.controller('ActionsGridController', [function() {
+.controller('ActionsGridController', ['$scope', 'Tabletop', ActionsGridController])
 
-}])
-
-.controller('ActionsHintController', [function() {
-
+.controller('ActionsHintController', ['$scope', function($scope) {
 }])
 
 .controller('ActionsListController', [function() {
 
 }]);
+
+/**
+  * $scope variables
+  * * categories	Array of the form [{Name: ".."}, {...}]
+  */
+function ActionsGridController($scope, Tabletop) {
+	$scope.categories = [];
+
+	Tabletop.then(function(sheets) {
+		// Get the Categories table
+		var table = sheets[0]["Categories"];
+		
+		// Put the categories on the scope
+		$scope.categories = table.all();
+    });
+}
+
+})();
--- app/theme/climateforchangetrial-565d2772ebad6419ed000001/app.js
+++ app/theme/climateforchangetrial-565d2772ebad6419ed000001/app.js
@@ -7,4 +7,18 @@ angular.module('c4cWebsite', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
-}]);
+}]).
+
+config(function(TabletopProvider){
+	// Configure the address of the Spreadsheet to use
+	TabletopProvider.setTabletopOptions({
+	  key: 'https://docs.google.com/spreadsheets/d/1PYC-NIMnCePaeWcCFsP5v-5YBQ1R2o6qu7quw6j784g/pubhtml',
+	  simpleSheet: false
+	  
+	});
+})
+.config(function($interpolateProvider){
+	// Change the default angular escape code as this clashes with liquid and
+	// causes all our inlines to disappear
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
